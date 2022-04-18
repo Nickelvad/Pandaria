6750dec7db669c0326a3f4bca6d81d8c7085b1ce
tstaiye <ts.taiye@live.com>
2022-04-18 03:02:52 +0200

    work started

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform targetTransform;
+    public float verticalAngle = 60f;
+    public float hortizontalAngle = 180f;
+    public float distance = 20f;
+
+
+    void LateUpdate()
+    {
+        if (this.targetTransform == null)
+        {
+            return;
+        }
+
+        var vector = Quaternion.AngleAxis(this.verticalAngle, Vector3.forward) * Vector3.right;
+        vector = Quaternion.AngleAxis(this.hortizontalAngle, Vector3.up) * vector;
+
+        var result = this.targetTransform.position + vector * distance;
+
+        this.transform.position = Vector3.Slerp(this.transform.position, result, 0.5f);
+        this.transform.LookAt(this.targetTransform);
+    }
+
+    public void SetTarget(Transform transform)
+    {
+        this.targetTransform = transform;
+    }
+}
---
+++ Assets/Scripts/CharacterMovementController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Pandaria.Joystick;
+
+public class CharacterMovementController : MonoBehaviour
+{
+    public float inputX = 0.0f;
+    public float inputY = 0.0f;
+    private Vector3 moveDirection;
+    public float speed = 3.0f;
+    public Joystick joystick;
+
+    public float jumpSpeed = 2.0f;
+
+    private Rigidbody rigidbody_;
+
+    public float distanceToCheck = 2f;
+    public bool isGrounded = true;
+    private bool oldIsGrouned = true;
+    private bool startedToJump = false;
+    private Vector3 jumpDirection;
+    private RaycastHit hit;
+
+    void Start()
+    {
+        rigidbody_ = transform.GetComponent<Rigidbody>();
+    }
+
+    private float GetHorizontal()
+    {
+        return joystick.Horizontal;
+    }
+
+    private float GetVertical()
+    {
+        return joystick.Vertical;
+    }
+
+    private bool CheckIfGrounded()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        return Physics.Raycast(ray, out hit, distanceToCheck);
+    }
+
+    void Update()
+    {
+        isGrounded = CheckIfGrounded();
+
+        inputY = this.GetVertical();
+        inputX = this.GetHorizontal();
+
+
+
+        moveDirection = new Vector3(inputX, 0f, inputY);
+        if (!isGrounded)
+        {
+            moveDirection = rigidbody_.transform.forward;
+        }
+        moveDirection.Normalize();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            startedToJump = true;
+            rigidbody_.AddForce(Vector2.up * jumpSpeed, ForceMode.Impulse);
+        }
+
+        if (moveDirection != Vector3.zero)
+        {
+            if (isGrounded)
+            {
+                Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+                rigidbody_.rotation = Quaternion.Slerp(rigidbody_.rotation, newRotation, Time.deltaTime * 5);
+            }
+
+            rigidbody_.MovePosition(rigidbody_.position + moveDirection * speed * Time.deltaTime);
+
+            // transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 5);
+
+            // if (isGrounded == true)
+            // {
+            //     transform.position += transform.forward * Time.deltaTime * speed;
+            // }
+            
+        }
+    }
+}
